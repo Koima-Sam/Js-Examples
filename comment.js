84902eda18e78b63594c6b412274c5ddbb1f8814
Koima-Sam <sam17koima@gmail.com>
2022-05-31 13:09:15 +0300

    commenting in js

---
+++ comment.js
@@ -0,0 +1,8 @@
+/*
+const lineOne = "Joy to the world";
+const lineTwo = "All the boys and girls";
+const lineThree = "Joy to the fishes in the deep blue sea"; 
+*/
+const lineFour = "Joy to you and me";
+
+lineFour; // => "Joy to you and me"
